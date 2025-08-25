eab09577476e266f53860f282c728b70d7a58840
leprof <prof.violet.club@gmail.com>
2025-08-25 22:31:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 Promise.all([
   fetch('europe.json').then(res => res.json()),
-  fetch('europe1.json').then(res => res.json())
+  fetch('europe1.json').then(res => res.json()),
+  fetch('europe2.json').then(res => res.json()),
+  fetch('europe3.json').then(res => res.json()),
+  fetch('europe4.json').then(res => res.json()),
 ])
 .then(([data1, data2]) => {
   const data = [...data1, ...data2]; // fusionne les deux tableaux
